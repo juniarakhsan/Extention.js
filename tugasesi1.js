ab93ffacb6fe7a6c6331bd8299f8babe7d2519af
juniarakhsan <124061925+juniarakhsan@users.noreply.github.com>
2023-09-29 23:14:16 +0700

    Add files via upload

---
+++ tugasesi1.js
@@ -0,0 +1,22 @@
+// Identitas
+const nama = "Juniar akhsan";
+const nim = "20220040085";
+
+console.log("Identitas : ");
+console.log("Nama : " + nama);
+console.log("Nim : " + nim);
+
+// Operasi Aritmatika Sederhana
+const angka1 = 80;
+const angka2 = 10;
+
+const tambah = angka1 + angka2;
+const kurang = angka1 - angka2;
+const kali = angka1 * angka2;
+const bagi = angka1 / angka2;
+
+console.log("Operasi Aritmatika : ");
+console.log(`${angka1} + ${angka2} = ${tambah}`);
+console.log(`${angka1} - ${angka2} = ${kurang}`);
+console.log(`${angka1} * ${angka2} = ${kali}`);
+console.log(`${angka1} / ${angka2} = ${bagi}`);
