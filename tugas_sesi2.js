e91017ca38a0f76d503d7688982e6e2ce24c02e0
juniarakhsan <124061925+juniarakhsan@users.noreply.github.com>
2023-10-03 18:16:57 +0700

    Add files via upload

---
+++ tugas_sesi2.js
@@ -0,0 +1,16 @@
+let firsnamame = "Juniar"
+let lastname = "Akhsan"
+let Age = 19
+let Ismarried = "Belum" == "Belum"
+
+let template = `firsname : ${firsnamame} 
+Lastname : ${lastname} 
+Age : ${Age} 
+Belum menikah : ${Ismarried}`
+console.log(template)
+
+// Pembuktian tipe data
+console.log(typeof firsnamame)
+console.log(typeof lastname)
+console.log(typeof Age)
+console.log(typeof Ismarried)
