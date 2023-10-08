597e9e43bc67b65b289c5946b175161418cc8bc9
juniarakhsan <124061925+juniarakhsan@users.noreply.github.com>
2023-10-08 11:19:46 +0700

    Add files via upload

---
+++ nama.js
@@ -0,0 +1,40 @@
+/*
+// Latihan Variable dan pengubahan variable kecuali cons
+var fullname = "Juniar akhsan"
+var asalkampus = "Universitas nusa putra"
+var asalprodi = "Teknik informatika"
+
+var fullname ;
+fullname = "san"
+console.log(fullname)
+console.log(asalkampus)
+console.log(asalprodi)
+console.log(fullname)
+
+// Latihan operator aritmatika
+let result = 1 + 2
+console.log("1 + 2 = " + result)
+let originalResult = result;
+
+result = result - 1
+console.log(originalResult + " - 1 =" + result)
+originalResult = result;
+
+result = result *2
+console.log(originalResult + " * 2 = " + result)
+originalResult = result;
+
+const name = "Juniar akhsan"
+const value = 80
+
+const template = `  Name : ${name}, Lulus : ${value >75}`
+
+console.info(template) // Name : Juniar akhsan, lulus : true
+*/
+const value1 = "1"
+const value2= 1
+const sum = value1 + value2
+
+console.log(`${parseInt("1.1")}`) // 1
+console.log(`${parseFloat("1.1")}`) // 1.1
+console.log(`${Number("1.1")}`) // 1.1
